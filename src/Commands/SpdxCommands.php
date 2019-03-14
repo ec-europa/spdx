b4d0e4ba5555f533fb020aa6423e9107683a6acb
Ilias Dimopoulos <idimopoulos@hotmail.com>
2019-03-14 17:10:28 +0200

    ISAICP-5130: Implement QA remarks.

--- src/Commands/SpdxCommands.php
+++ src/Commands/SpdxCommands.php
@@ -1,11 +1,16 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Drupal\spdx\Commands;
 
 use Composer\Autoload\ClassLoader;
 use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Database\Database;
+use Drupal\Driver\Database\sparql\Connection;
+use Drupal\rdf_entity\RdfEntityGraphStoreTrait;
 use Drush\Commands\DrushCommands;
+use Drush\Exceptions\UserAbortException;
 use EasyRdf\Graph;
 use EasyRdf\GraphStore;
 use ReflectionClass;
@@ -14,6 +19,8 @@ use ReflectionClass;
  * Class SpdxCommands
  */
 class SpdxCommands extends DrushCommands {
+
+  use RdfEntityGraphStoreTrait;
 
   /**
    * Imports the SPDX licenses into the default SPARQL database.
@@ -30,29 +37,30 @@ class SpdxCommands extends DrushCommands {
    * @usage spdx:import "http://example.com/licenses" --clean
    *   Imports the licenses into the graph but also cleans the graph first.
    *
-   * @throws \ReflectionException
-   *   Thrown if the class does not exist.
+   * @throws \Drush\Exceptions\UserAbortException
+   *   Thrown if the user cancels the command.
    */
-  public function importLicenses($graph_uri, $options = ['clean' => FALSE]) {
+  public function importLicenses(string $graph_uri, array $options = ['clean' => FALSE]): void {
     if (!UrlHelper::isValid($graph_uri, ['absolute' => TRUE])) {
       throw new \InvalidArgumentException('Graph URI is not a valid URL');
     }
 
-    $reflextion = new ReflectionClass(ClassLoader::class);
+    $message = $options['clean'] ?
+      'This will clean the "%graph" graph and re import all licenses. Are you sure you want to proceed?' :
+      'This will import all license data into the "%graph" graph. Are you sure you want to continue?';
 
-    // Dirname with 2 as the second parameter, will return the full path
-    // including the ./vendor/ subdir
-    $directory = dirname($reflextion->getFilename(), 2);
-    $dir_parts = [$directory, 'spdx', 'license-list-data', 'rdfxml'];
+    if (!$this->io()->confirm(dt($message, ['%graph' => $graph_uri]))) {
+      throw new UserAbortException();
+    }
+
+    $vendor_dir = $this->getConfig()->get('drush.vendor-dir');
+    $dir_parts = [$vendor_dir, 'spdx', 'license-list-data', 'rdfxml'];
     $spdx_source = implode(DIRECTORY_SEPARATOR, $dir_parts) . DIRECTORY_SEPARATOR;
 
-    /** @var \Drupal\Driver\Database\joinup_sparql\Connection $connection */
-    $connection = \Drupal::service('sparql_endpoint');
-    $connection_options = $connection->getConnectionOptions();
-    $connect_string = "http://{$connection_options['host']}:{$connection_options['port']}/sparql-graph-crud";
-
+    $graph_store = $this->createGraphStore();
     if ($options['clean']) {
-      $connection->query("CLEAR GRAPH <{$graph_uri}>;");
+      $client = Connection::open(Database::getConnectionInfo('sparql_default')['default']);
+      $client->clear($graph_uri);
       $this->logger()->success(dt('Graph has been cleaned.'));
     }
 
@@ -64,7 +72,7 @@ class SpdxCommands extends DrushCommands {
     }
 
     $count = 0;
-    foreach (glob($spdx_source . '*.rdf') as $rdf_file_path) {
+    foreach ($rdf_files as $rdf_file_path) {
       $graph = new Graph($graph_uri);
       $graph->parseFile($rdf_file_path);
       // There is a file that includes all the licenses and we don't need to
@@ -74,9 +82,7 @@ class SpdxCommands extends DrushCommands {
       if (count($licenses) !== 1) {
         continue;
       }
-      $graph_store = new GraphStore($connect_string);
       $graph_store->insert($graph);
-
       $count++;
       $this->logger()->success(dt('Imported file %file.', [
         '%file' => $rdf_file_path
